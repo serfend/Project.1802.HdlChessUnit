d38e8d40282681d41d2668b589c7f86884e6716c
aserfend <liuchangm7j@vip.qq.com>
2018-02-18 19:27:22 +0800

    建立项目

---
+++ HdlChessUnit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace HdlChessUnit
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+		
+	}
+}
